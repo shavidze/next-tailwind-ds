ec6cd03369727c097517d90774d6f8b1d4f0f68e
Saba Shavidze <sabashavidze@gmail.com>
2023-06-02 15:44:31 +0400

    place the ButtonOrLink component inside Button folder

--- src/app/ui/Button/Button.tsx
+++ src/app/ui/Button/Button.tsx
@@ -1,5 +1,5 @@
 import { cva, VariantProps } from 'class-variance-authority';
-import { ButtonOrLink, Props as ButtonOrLinkProps } from '../ButtonOrLink';
+import { ButtonOrLink, Props as ButtonOrLinkProps } from './ButtonOrLink';
 
 const buttonStyles = cva(
   'flex items-center justify-center px-4 py-2 rounded font-medium focus:outline-none focus:ring-2 focus:ring-offset-white dark:focus:ring-offset-black focus:ring-offset-1 disabled:opacity-60 disabled:pointer-events-none hover:bg-opacity-80',
