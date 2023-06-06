034105fe0cf9389e8b78313c1bfd33667bbd90aa
Saba Shavidze <sabashavidze@gmail.com>
2023-06-06 18:48:51 +0400

    Destructure the input variant in component

--- src/app/ui/Text/Text.tsx
+++ src/app/ui/Text/Text.tsx
@@ -1,4 +1,5 @@
 import { cva, VariantProps } from 'class-variance-authority';
+import { ReactNode } from 'react';
 
 const textStyles = cva([], {
   variants: {
@@ -22,4 +23,15 @@ export interface TextProps extends Omit<TextStylesProps, 'size' | 'weigth'> {
   variant: `${NonNullable<TextStylesProps['size']>} / ${NonNullable<
     TextStylesProps['weight']
   >}`;
+  children?: ReactNode;
+}
+
+export function Text({ variant, children, ...props }: TextProps) {
+  const [size, weight] = variant.split('/') as [
+    TextStylesProps['size'],
+    TextStylesProps['weight']
+  ];
+  return (
+    <div className={textStyles({ size, weight, ...props })}>{children}</div>
+  );
 }
