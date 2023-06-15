f1f2ea396a1c10f844ec92780e80ff310ca06c10
Saba Shavidze <sabashavidze@gmail.com>
2023-06-15 14:25:02 +0400

    add toggle base component

---
+++ src/app/ui/Toggle/Toggle.stories.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Meta, StoryObj } from '@storybook/react';
+import { Toggle } from './Toggle';
+
+export default {
+  title: 'UI/Toggle',
+  component: Toggle,
+  args: {},
+} as Meta<typeof Toggle>;
+type Story = StoryObj<typeof Toggle>;
+
+export const TogglePrimary: Story = {
+  args: {},
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,12 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    colors: {
+      surface: {
+        50: '#E4E6EE',
+        900: '#242C46',
+      },
+    },
   },
   plugins: [],
 };
